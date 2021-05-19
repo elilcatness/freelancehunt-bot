a69f70f483060dade8cb2171478972c34ddfe0db
elilcatness <elilcatness@gmail.com>
2021-05-20 00:08:21 +0300

    migrated to heroku db

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from parse import FreelanceHunt
 
 def job(context):
     bot = context.job.context.bot
-    session = db_session.create_session()
-    mail = session.query(Mail).get(context.job.context.user_data['chat_id'])
-    parser = context.job.context.user_data['parser']
-    updates = parser.get_updates(mail.last_id)
-    if updates:
-        mail.last_id = updates[0]['id']
-        session.merge(mail)
-        session.commit()
+    with db_session.create_session() as session:
+        mail = session.query(Mail).get(context.job.context.user_data['chat_id'])
+        parser = context.job.context.user_data['parser']
+        updates = parser.get_updates(mail.last_id)
+        if updates:
+            mail.last_id = updates[0]['id']
+            session.merge(mail)
+            session.commit()
     for update in updates[::-1]:
         message = '\n'.join([f'<b>{key}</b>: {val}' for key, val in list(update.items())[2:]])
         markup = InlineKeyboardMarkup([[InlineKeyboardButton('Открыть', url=update['url'])]])
@@ -32,9 +32,9 @@ def start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     if context.job_queue.get_jobs_by_name(str(chat_id)):
         return update.message.reply_text('Рассылка уже включена')
-    session = db_session.create_session()
-    session.add(Mail(id=chat_id))
-    session.commit()
+    with db_session.create_session() as session:
+        session.add(Mail(id=chat_id))
+        session.commit()
     context.user_data['parser'] = FreelanceHunt(os.getenv('fh_token'))
     context.user_data['chat_id'] = chat_id
     context.job_queue.run_repeating(job, timedelta(seconds=10), timedelta(seconds=1),
@@ -44,26 +44,26 @@ def start(update: Update, context: CallbackContext):
 
 def stop(update, context):
     chat_id = update.message.chat_id
-    session = db_session.create_session()
-    mail = session.query(Mail).get(chat_id)
-    if not mail:
-        return update.message.reply_text('Рассылка не была включена')
-    for job in context.job_queue.get_jobs_by_name(str(chat_id)):
-        job.schedule_removal()
-    session.delete(mail)
-    session.commit()
+    with db_session.create_session() as session:
+        mail = session.query(Mail).get(chat_id)
+        if not mail:
+            return update.message.reply_text('Рассылка не была включена')
+        for job in context.job_queue.get_jobs_by_name(str(chat_id)):
+            job.schedule_removal()
+        session.delete(mail)
+        session.commit()
     update.message.reply_text('Рассылка была успешно выключена')
 
 
 def start_last_mails(dispatcher, bot):
-    session = db_session.create_session()
-    parser = FreelanceHunt(os.getenv('fh_token'))
-    for mail in session.query(Mail).all():
-        context = CallbackContext(dispatcher)
-        context._bot = bot
-        context._user_data = {'parser': parser, 'chat_id': mail.id}
-        context.job_queue.run_repeating(job, timedelta(seconds=10), timedelta(seconds=1),
-                                        context=context, name=str(mail.id))
+    with db_session.create_session() as session:
+        parser = FreelanceHunt(os.getenv('fh_token'))
+        for mail in session.query(Mail).all():
+            context = CallbackContext(dispatcher)
+            context._bot = bot
+            context._user_data = {'parser': parser, 'chat_id': mail.id}
+            context.job_queue.run_repeating(job, timedelta(seconds=10), timedelta(seconds=1),
+                                            context=context, name=str(mail.id))
 
 
 def main():
@@ -76,5 +76,5 @@ def main():
 
 
 if __name__ == '__main__':
-    db_session.global_init(os.path.join('db', 'mailings.db'))
+    db_session.global_init(os.getenv('DATABASE_URL'))
     main()
