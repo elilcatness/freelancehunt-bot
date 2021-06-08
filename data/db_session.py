fa6251be81870adf22c98b8fdbce520d53671cda
elilcatness <elilcatness@gmail.com>
2021-06-08 23:43:48 +0300

    changed db type

--- data/db_session.py
+++ data/db_session.py
@@ -1,21 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.pool import NullPool
 
 SqlAlchemyBase = declarative_base()
 __factory = None
 
 
-def global_init(db_file):
-    db_file = db_file.strip()
+def global_init(url):
+    url = url.strip()
     global __factory
     if __factory:
         return
-    if not db_file:
-        raise Exception('Некорректный файл БД')
-    connection_str = 'sqlite:///%s?check_same_thread=False' % db_file
-    engine = create_engine(connection_str, echo=False)
-    __factory = sessionmaker(bind=engine)
+    if not url:
+        raise Exception('Некорректный адрес БД')
+    engine = create_engine(url, echo=False)
+    __factory = sessionmaker(bind=engine, poolclass=NullPool)
     from . import __all_models
     SqlAlchemyBase.metadata.create_all(engine)
 
